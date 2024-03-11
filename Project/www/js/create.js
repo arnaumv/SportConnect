5b4065d510c7df434c95772862b3839b6b5c410b
arnaumv <95907269+arnaumv@users.noreply.github.com>
2024-03-11 22:21:40 +0100

    Update create.js

--- Project/www/js/create.js
+++ Project/www/js/create.js
@@ -1,15 +1,33 @@
 $(document).ready(function(){
-    $('#btnEnviar').click(function(){
+  $('#btnEnviar').click(function(){
       var titulo = $('#titulo').val();
       var tipoDeporte = $('#tipoDeporte').val();
       var fecha = $('#fecha').val();
+      var hora = $('#hora').val();
       var ubicacion = $('#ubicacion').val();
       var descripcion = $('#descripcion').val();
-      console.log('Título: ' + titulo);
-      console.log('Tipo de Deporte: ' + tipoDeporte);
-      console.log('Fecha: ' + fecha);
-      console.log('Ubicación: ' + ubicacion);
-      console.log('Descripción: ' + descripcion);
-    });
+
+      $.ajax({
+        url: 'http://localhost:8000/events/',  // URL de tu aplicación Django local
+        type: 'POST',
+          data: {
+              title: titulo,
+              sport: tipoDeporte,
+              date: fecha,
+              time: hora,
+              location: ubicacion,
+              description: descripcion
+          },
+          success: function(result) {
+              // maneja el éxito
+              console.log(result);
+              alert('Evento creado con éxito.');  // Mensaje de éxito
+          },
+          error: function(error) {
+              // maneja el error
+              console.log(error);
+              alert('Hubo un error al crear el evento.');  // Mensaje de error
+          }
+      });
   });
-  
+});
