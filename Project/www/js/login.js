146750684839e2a50dfff9d37baa337bb3c772c6
arnaumv <95907269+arnaumv@users.noreply.github.com>
2024-03-12 11:55:43 +0100

    Update login.js

--- Project/www/js/login.js
+++ Project/www/js/login.js
@@ -1,8 +1,30 @@
-$(document).ready(function(){
-    $('#btnEnviar').click(function(){
-      var nombre = $('#nombre').val();
-      var contrasena = $('#contrasena').val();
-      console.log('Nombre: ' + nombre);
-      console.log('Contraseña: ' + contrasena);
+$(document).ready(function() {
+    $('#btnEnviar').click(function(e) {
+        e.preventDefault();
+
+        var nombre = $('#nombre').val();
+        var contrasena = $('#contrasena').val();
+
+        $.ajax({
+            url: 'http://localhost:8000/api-token-auth/',  // URL de tu endpoint de autenticación
+            method: 'POST',
+            data: {
+                username: nombre,
+                password: contrasena
+            },
+            success: function(data) {
+                console.log('Token: ' + data.token);
+                // Guarda el token en el almacenamiento local del navegador
+                window.localStorage.setItem('token', data.token);
+                // Redirige al usuario a la página principal de tu aplicación
+                window.location.href = 'Index.html';
+            },
+            error: function(xhr, status, error) {
+                console.log('Error: ' + error);
+                console.log('Status: ' + status);
+                console.log(xhr);
+                // Aquí puedes manejar el error, por ejemplo mostrando un mensaje al usuario
+            }
+        });
     });
 });
