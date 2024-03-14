33d9be4a94bc2922a94581804bc7057a9dc24f49
arnaumv <95907269+arnaumv@users.noreply.github.com>
2024-03-14 12:34:05 +0100

    LOGIN CORRECTO (PERO CON USERNAME)

--- Project/www/js/login.js
+++ Project/www/js/login.js
@@ -2,28 +2,27 @@ $(document).ready(function() {
     $('#btnEnviar').click(function(e) {
         e.preventDefault();
 
-        var email = $('#email').val();
+        var name = $('#name').val();
         var contrasena = $('#contrasena').val();
 
         $.ajax({
-            url: 'http://localhost:8000/login/',  // URL de tu endpoint de autenticación
+            url: 'http://localhost:8000/api/login/',  // URL de tu endpoint de autenticación
             method: 'POST',
-            data: {
-                email: email,
+            contentType: 'application/json',  // Asegúrate de que estás enviando los datos como JSON
+            data: JSON.stringify({  // Convierte tus datos a JSON
+                name: name,
                 password: contrasena
-            },
+            }),
             success: function(data) {
                 console.log('Token: ' + data.token);
-                // Guarda el token en el almacenamiento local del navegador
                 window.localStorage.setItem('token', data.token);
-                // Redirige al usuario a la página principal de tu aplicación
+                alert('Login successful!');  // Muestra un alerta cuando el inicio de sesión es exitoso
                 window.location.href = '/Index.html';
             },
             error: function(xhr, status, error) {
                 console.log('Error: ' + error);
                 console.log('Status: ' + status);
                 console.log(xhr);
-                // Aquí puedes manejar el error, por ejemplo mostrando un mensaje al usuario
                 alert('Error: ' + error + '. ' + 'Status: ' + status);
             }
         });
