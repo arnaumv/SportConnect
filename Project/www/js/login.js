1f4fee2abeab5744e17be3ee640fafcf8d1d2533
arnaumv <arnaumestre02@gmail.com>
2024-03-18 18:46:06 +0100

    INDEX Y LOGIN MODIFICADOS

--- Project/www/js/login.js
+++ Project/www/js/login.js
@@ -1,42 +1,40 @@
 $(document).ready(function() {
-    $('#btnEnviar').click(function(e) {
+    $('#btnSubmit').click(function(e) {
         e.preventDefault();
-        console.log("login");
-        var email = $('#email').val();
-        var contrasena = $('#contrasena').val();
-        var areErrors = false;
+        var username = $('#username').val();
+        var password = $('#password').val();
+        var hasErrors = false;
 
-        // Validación del campo de correo electrónico
-        var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Expresión regular para validar el formato del correo electrónico
-        if (!emailPattern.test(email)) {
-            $('#error_email').html('Introduce un correo electrónico válido');
-            areErrors = true;
+        // Validación del campo de nombre de usuario
+        if (username === '') {
+            $('#error_username').html('Por favor, introduce tu nombre de usuario');
+            hasErrors = true;
         } else {
-            $('#error_email').html('');
+            $('#error_username').html('');
         }
 
         // Validación del campo de contraseña
-        if (contrasena.length < 8 || contrasena.length > 128) {
-            $('#error_contrasena').html('La contraseña debe tener entre 8 y 128 caracteres');
-            areErrors = true;
+        if (password.length < 8 || password.length > 128) {
+            $('#error_password').html('La contraseña debe tener entre 8 y 128 caracteres');
+            hasErrors = true;
         } else {
-            $('#error_contrasena').html('');
+            $('#error_password').html('');
         }
 
         // Si no hay errores, puedes enviar la solicitud AJAX
-        if (!areErrors) {
+        if (!hasErrors) {
             $.ajax({
                 url: 'http://localhost:8000/login/',
                 method: 'POST',
                 data: JSON.stringify({
-                    email: email,
-                    contrasena: contrasena
+                    username: username,
+                    password: password
                 }),
                 contentType: 'application/json',
                 success: function(data) {
-                    console.log('Response data:', data);
                     console.log('Login successful:', data);
-                    localStorage.setItem('token', data.token);
+                    // Redirige al usuario a index.html
+                    window.location.href = 'Index.html';
                 },
                 error: function(error) {
                     console.log('Error:', error);
