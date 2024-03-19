624a7d016e5df76c4960ca6c33f47288882f0899
MehdiRegg777 <96047417+MehdiRegg777@users.noreply.github.com>
2024-03-19 19:18:36 +0100

    fallos

--- Project/www/js/resetPass.js
+++ Project/www/js/resetPass.js
@@ -1,28 +1,30 @@
-$(document).on('click', '#btnEnviar', function() {
-    $('#btnEnviar').click(function(){
-      // Restablecer los mensajes de error
-      $('.error').text('');
+$(document).ready(function() {
 
-      // Obtener los valores de los campos del formulario
-      var contrasena = $('#contrasena').val().trim();
-      var repetirContrasena = $('#repetir-contrasena').val().trim();
-      
-      // Variable para verificar si hay errores
-      var hayErrores = false;
+    $(document).on('click', '#btnEnviarPass', function() {
 
-      // Validación de la longitud y coincidencia de las contraseñas
-      if(contrasena.length < 8){
-          $('#error_contrasena').text('La contraseña debe tener al menos 8 caracteres');
-          hayErrores = true;
-      } else if(contrasena !== repetirContrasena){
-          $('#error_repetir_contrasena').text('Las contraseñas no coinciden');
-          hayErrores = true;
-      }
+        // Restablecer los mensajes de error
+        $('.error').text('');
 
-      // Si no hay errores, enviar los datos
-      if(!hayErrores){
-          // Aquí puedes agregar la lógica para enviar el formulario o realizar otras acciones necesarias
-          console.log("Formulario enviado");
-      }
-  });
+            // Obtener los valores de los campos del formulario
+            var contrasena = $('#contrasena').val().trim();
+            var repetirContrasena = $('#repetir-contrasena').val().trim();
+            
+            // Variable para verificar si hay errores
+            var hayErrores = false;
+
+            // Validación de la longitud y coincidencia de las contraseñas
+        if(contrasena.length < 8){
+            $('#error_contrasena').text('La contraseña debe tener al menos 8 caracteres');
+            hayErrores = true;
+        } else if(contrasena !== repetirContrasena){
+            $('#error_repetir_contrasena').text('Las contraseñas no coinciden');
+            hayErrores = true;
+        }
+
+        // Si no hay errores, enviar los datos
+        if(!hayErrores){
+            // Aquí puedes agregar la lógica para enviar el formulario o realizar otras acciones necesarias
+            console.log("Formulario enviado");
+        }
+    });
 });
--- Project/www/js/resetPassMail.js
+++ Project/www/js/resetPassMail.js
@@ -1,25 +1,41 @@
-$(document).on('click', '#btnEnviar', function() {
-    $('#btnEnviar').click(function() {
-      // Restablecer los mensajes de error
-      $('.error').text('');
+$(document).ready(function() {
+    $('#email').on('input', function() {
+        console.log("escribiendo...")
+        // Restablecer los mensajes de error
+        $('.error').text('');
 
-      // Obtener el valor del campo de correo electrónico
-      var email = $('#email').val().trim();
+        // Obtener el valor del campo de correo electrónico
+        var email = $(this).val().trim();
 
-      // Variable para verificar si hay errores
-      var hayErrores = false;
+        // Validación del correo electrónico
+        var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailPattern.test(email)) {
+            $('#error_email').text('Introduce un correo electrónico válido');
+        }
+    });
 
-      // Validación del correo electrónico
-      var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      if (!emailPattern.test(email)) {
-          $('#error_email').text('Introduce un correo electrónico válido');
-          hayErrores = true;
-      }
+    $('#btnEnviarMail').click(function() {
+        // Restablecer los mensajes de error
+        $('.error').text('');
 
-      // Si no hay errores, puedes continuar con el proceso
-      if (!hayErrores) {
-          // Aquí puedes agregar la lógica para enviar la solicitud o realizar otras acciones necesarias
-          console.log("Formulario enviado");
-      }
-  });
+        // Obtener el valor del campo de correo electrónico
+        var email = $('#email').val().trim();
+
+        // Variable para verificar si hay errores
+        var hayErrores = false;
+
+        // Validación del correo electrónico
+        var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailPattern.test(email)) {
+            $('#error_email').text('Introduce un correo electrónico válido');
+            hayErrores = true;
+        }
+
+        // Si no hay errores, puedes continuar con el proceso
+        if (!hayErrores) {
+            // Aquí puedes agregar la lógica para enviar la solicitud AJAX
+        console.log("ejecutar ajax");
+        }
+    });
 });
+
