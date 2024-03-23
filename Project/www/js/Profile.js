4fc76830fe431ffa7fc0ea77e24681b2139b0e5d
MehdiRegg777 <mregbaoui.cf@iesesteveterradas.cat>
2024-03-23 14:55:42 +0100

    EventosPerfil

--- Project/www/js/Profile.js
+++ Project/www/js/Profile.js
@@ -26,63 +26,68 @@ function onDeviceReady() {
         window.location.href = 'EditProfile.html';
     });
     
-    $(document).on('pageinit', function() {
-        var json_url = "Events.json";
-
-        // Función para mostrar eventos activos o finalizados según la categoría seleccionada
-        function mostrarEventosPorCategoria(categoriaSeleccionada) {
-            var eventsList = $('.events-list');
-            eventsList.empty(); // Limpiar la lista antes de agregar los eventos
-
-            // Cargar el JSON externo y mostrar eventos basados en la categoría seleccionada
-            $.getJSON(json_url, function(eventos) {
-                eventos.forEach(function(evento) {
-                    var currentDate = Date.now();
-                    var eventDate = new Date(evento.fecha).getTime();
-
-                    if ((categoriaSeleccionada === "Activos" && eventDate >= currentDate) ||
-                        (categoriaSeleccionada === "Finalizados" && eventDate < currentDate)) {
-                        agregarEvento(evento);
-                    }
-                });
-            });
-
-            // Marcar el botón activo correspondiente
-            $('.events-buttons button').removeClass('active');
-            $('#' + categoriaSeleccionada.toLowerCase() + '-events-btn').addClass('active');
-        }
-
-        // Agregar evento al DOM
-        function agregarEvento(evento) {
-            var eventHtml = '<div class="event" data-categoria="' + evento.categoria + '">';
-            eventHtml += '<img src="' + evento.imagen + '" alt="Imagen del Evento">';
-            eventHtml += '<h2>' + evento.titulo + '</h2>';
-            eventHtml += '<p>Fecha: ' + evento.fecha + '</p>';
-            eventHtml += '<p>Ubicación: ' + evento.ubicacion + '</p>';
-            eventHtml += '<a href="' + evento.enlace + '">';
-            eventHtml += '<button class="join-btn">Unirse al Evento</button>';
-            eventHtml += '</a>';
-            eventHtml += '</div>';
-            $('.events-list').append(eventHtml);
-        }
-
-        // Manejar clic en botón "Activos"
-        $('#active-events-btn').off('click').on('click', function() {
-            mostrarEventosPorCategoria("Activos");
-        });
-
-        // Manejar clic en botón "Finalizados"
-        $('#finished-events-btn').off('click').on('click', function() {
-            mostrarEventosPorCategoria("Finalizados");
-        });
-
-        // Mostrar eventos activos al cargar la página, solo si no se han cargado antes
-        if (!eventosCargados) {
-            setTimeout(function() {
-                mostrarEventosPorCategoria("Activos");
-                $('#active-events-btn').addClass('active');
-                eventosCargados = true;
-            }, 200);
-        }
-    });
+    
 }
+$(document).ready(function() {
+    var json_url = "Events.json";
+    console.log("eventsprofil");
+    // Función para mostrar eventos activos o finalizados según la categoría seleccionada
+    function mostrarEventosPorCategoria(categoriaSeleccionada) {
+        var eventsList = $('.events-list');
+        eventsList.empty(); // Limpiar la lista antes de agregar los eventos
+
+        // Cargar el JSON externo y mostrar eventos basados en la categoría seleccionada
+        $.getJSON(json_url, function(eventos) {
+            console.log(eventos);
+            eventos.forEach(function(evento) {
+                var currentDate = Date.now();
+                var eventDate = new Date(evento.fecha).getTime();
+
+                if ((categoriaSeleccionada === "Activos" && eventDate >= currentDate) ||
+                    (categoriaSeleccionada === "Finalizados" && eventDate < currentDate)) {
+                        console.log("agregar");
+                    agregarEvento(evento);
+                }
+            });
+        });
+
+        // Marcar el botón activo correspondiente
+        $('.events-buttons button').removeClass('active');
+        $('#' + categoriaSeleccionada.toLowerCase() + '-events-btn').addClass('active');
+    }
+
+    // Agregar evento al DOM
+    function agregarEvento(evento) {
+        console.log("añadirlos");
+        var eventHtml = '<div class="event" data-categoria="' + evento.categoria + '">';
+        eventHtml += '<img src="' + evento.imagen + '" alt="Imagen del Evento">';
+        eventHtml += '<h2>' + evento.titulo + '</h2>';
+        eventHtml += '<p>Fecha: ' + evento.fecha + '</p>';
+        eventHtml += '<p>Ubicación: ' + evento.ubicacion + '</p>';
+        eventHtml += '<a href="' + evento.enlace + '">';
+        eventHtml += '<button class="join-btn">Unirse al Evento</button>';
+        eventHtml += '</a>';
+        eventHtml += '</div>';
+        $('.events-list').append(eventHtml);
+    }
+
+    // Manejar clic en botón "Activos"
+    $('#active-events-btn').off('click').on('click', function() {
+        console.log("ookkactivos")
+        mostrarEventosPorCategoria("Activos");
+    });
+
+    // Manejar clic en botón "Finalizados"
+    $('#finished-events-btn').off('click').on('click', function() {
+        mostrarEventosPorCategoria("Finalizados");
+    });
+
+    // Mostrar eventos activos al cargar la página, solo si no se han cargado antes
+    if (!eventosCargados) {
+        setTimeout(function() {
+            mostrarEventosPorCategoria("Activos");
+            $('#active-events-btn').addClass('active');
+            eventosCargados = true;
+        }, 200);
+    }
+});
