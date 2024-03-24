eb67312ae53e35355f48a3abeac3c9f53f8ce6f1
arnaumv <95907269+arnaumv@users.noreply.github.com>
2024-03-24 18:35:26 +0100

    cambios en el profile.js

--- Project/www/js/Profile.js
+++ Project/www/js/Profile.js
@@ -69,11 +69,17 @@ $(document).ready(function() {
     // Agregar evento al DOM
     function agregarEvento(evento) {
         console.log("añadirlos");
+        var currentDate = Date.now();
+        var eventDate = new Date(evento.date).getTime();
         var eventHtml = '<div class="event" data-categoria="' + evento.sport + '">';
         eventHtml += '<img src="' + evento.image_path + '" alt="Imagen del Evento">';  
         eventHtml += '<h2>' + evento.title  + '</h2>';
         eventHtml += '<p>Fecha: ' + evento.date + '</p>';
         eventHtml += '<p>Ubicación: ' + evento.location  + '</p>';
+        // Add "View" button for active events
+        if (eventDate >= currentDate) {
+            eventHtml += '<button onclick="window.location.href=\'infoEvent.html?eventId=' + evento.id + '\'">Ver</button>';
+        }
         eventHtml += '</div>';
         $('.events-list').append(eventHtml);
     }
