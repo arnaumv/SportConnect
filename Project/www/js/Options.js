6455dfbecfd9449f4e8bdad6620de56db50a75d6
Xavi Martinez <xaavii0711@gmail.com>
2024-04-03 19:05:27 +0200

    CSS & new pages

--- Project/www/js/Options.js
+++ Project/www/js/Options.js
@@ -5,6 +5,19 @@ $(document).ready(function() {
     $('#deleteX').on('click', function() {
         window.location.href = 'Profile.html';
     });
+
+    $('#Edit_Profile').on('click', function() {
+        window.location.href = 'EditProfile.html';
+    });
+
+    $('#a_terms').on('click', function() {
+        window.location.href = 'Terms.html';
+    });
+
+    $('#a_privac').on('click', function() {
+        window.location.href = 'Privacy.html';
+    });
+    
     
 
 });
