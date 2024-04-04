e5027e77eab531207aaa6b79751a2b3b6d1faca3
arnaumv <95907269+arnaumv@users.noreply.github.com>
2024-04-04 12:45:11 +0200

    Validaciones crear usuario modificadas

--- Project/www/js/newUser.js
+++ Project/www/js/newUser.js
@@ -22,69 +22,69 @@ $(document).ready(function() {
         window.location.href = 'Index.html';
     });
 
-  // Validación del nombre de usuario al perder el foco
-  $('.formNewUser #username').on('focusout', function() {
-      var username = $(this).val().trim();
-      if(username === ''){
-          $('#error_username').text('Por favor, introduce tu nombre de usuario');
-          $(this).addClass('error-input'); // Agregar clase de error al input
+    // Validación del nombre de usuario mientras se está escribiendo y al perder el foco
+    $('.formNewUser #username').on('input focusout', function() {
+        var username = $(this).val().trim();
+        if(username === ''){
+            $('#error_username').text('Por favor, introduce tu nombre de usuario');
+            $(this).addClass('error-input'); // Agregar clase de error al input
         } else {
-          $('#error_username').text('');
-          $(this).removeClass('error-input'); // Eliminar clase de error del input
+            $('#error_username').text('');
+            $(this).removeClass('error-input'); // Eliminar clase de error del input
         }
-  });
+    });
 
-  // Validación del correo electrónico al perder el foco
-  $('.formNewUser #email').on('focusout', function() {
-      var email = $(this).val().trim();
-      if(email === ''){
-          $('#error_email').text('Por favor, introduce tu correo electrónico');
-          $(this).addClass('error-input'); // Agregar clase de error al input
-      } else if(!isValidEmail(email)){
-          $('#error_email').text('El correo electrónico no es válido');
-          $(this).addClass('error-input'); // Agregar clase de error al input
-      } else {
-          $('#error_email').text('');
-          $(this).removeClass('error-input'); // Eliminar clase de error del input
-      }
-  });
+    // Validación del correo electrónico mientras se está escribiendo y al perder el foco
+    $('.formNewUser #email').on('input focusout', function() {
+        var email = $(this).val().trim();
+        if(email === ''){
+            $('#error_email').text('Por favor, introduce tu correo electrónico');
+            $(this).addClass('error-input'); // Agregar clase de error al input
+        } else if(!isValidEmail(email)){
+            $('#error_email').text('El correo electrónico no es válido');
+            $(this).addClass('error-input'); // Agregar clase de error al input
+        } else {
+            $('#error_email').text('');
+            $(this).removeClass('error-input'); // Eliminar clase de error del input
+        }
+    });
 
-  // Validación de la contraseña al perder el foco
-  $('.formNewUser #password').on('focusout', function() {
-      var password = $(this).val().trim();
-      if(password.length < 8 || password.length > 128){
-          $('#error_password').text('La contraseña debe tener entre 8 y 128 caracteres');
-          $(this).addClass('error-input'); // Agregar clase de error al input
-      } else {
-          $('#error_password').text('');
-          $(this).removeClass('error-input'); // Eliminar clase de error del input
-      }
-  });
+    // Validación de la contraseña mientras se está escribiendo y al perder el foco
+    $('.formNewUser #password').on('input focusout', function() {
+        var password = $(this).val().trim();
+        if(password.length < 8 || password.length > 128){
+            $('#error_password').text('La contraseña debe tener entre 8 y 128 caracteres');
+            $(this).addClass('error-input'); // Agregar clase de error al input
+        } else {
+            $('#error_password').text('');
+            $(this).removeClass('error-input'); // Eliminar clase de error del input
+        }
+    });
 
-  // Confirmación de la contraseña al perder el foco
-  $('.formNewUser #confirm_password').on('focusout', function() {
-      var password = $('.formNewUser #password').val().trim();
-      var confirm_password = $(this).val().trim();
-      if(password !== confirm_password){
-          $('#error_confirm_password').text('Las contraseñas no coinciden');
-          $(this).addClass('error-input'); // Agregar clase de error al input
-      } else {
-          $('#error_confirm_password').text('');
-          $(this).removeClass('error-input'); // Eliminar clase de error del input
-      }
-  });
+    // Confirmación de la contraseña mientras se está escribiendo y al perder el foco
+    $('.formNewUser #confirm_password').on('input focusout', function() {
+        var password = $('.formNewUser #password').val().trim();
+        var confirm_password = $(this).val().trim();
+        if(password !== confirm_password){
+            $('#error_confirm_password').text('Las contraseñas no coinciden');
+            $(this).addClass('error-input'); // Agregar clase de error al input
+        } else {
+            $('#error_confirm_password').text('');
+            $(this).removeClass('error-input'); // Eliminar clase de error del input
+        }
+    });
 
-  // Validación de la ciudad al perder el foco
-  $('.formNewUser #city').on('focusout', function() {
-      var city = $(this).val().trim();
-      if(city.length < 2){
-          $('#error_city').text('La ciudad debe tener al menos 2 caracteres');
-          $(this).addClass('error-input'); // Agregar clase de error al input
-      } else {
-          $('#error_city').text('');
-          $(this).removeClass('error-input'); // Eliminar clase de error del input
-      }
-  });
+    // Validación de la ciudad mientras se está escribiendo y al perder el foco
+    $('.formNewUser #city').on('input focusout', function() {
+        var city = $(this).val().trim();
+        if(city.length < 2){
+            $('#error_city').text('La ciudad debe tener al menos 2 caracteres');
+            $(this).addClass('error-input'); // Agregar clase de error al input
+        } else {
+            $('#error_city').text('');
+            $(this).removeClass('error-input'); // Eliminar clase de error del input
+        }
+    });
 
   // Validación de la fecha de nacimiento al perder el foco
   $('.formNewUser #birthdate').on('focusout', function() {
