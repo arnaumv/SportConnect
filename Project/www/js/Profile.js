cc247e1a0823401a099817639713af60d5161b85
arnaumv <95907269+arnaumv@users.noreply.github.com>
2024-05-14 11:39:09 +0200

    LLAMADA AL JS MODIFICADA EN EL PROFILE

--- Project/www/js/Profile.js
+++ Project/www/js/Profile.js
@@ -47,6 +47,62 @@ function verEvento(eventId) {
 }
 
 $(document).ready(function () {
+
+
+
+
+
+    var storedUsername = localStorage.getItem('username');
+    // Si no hay datos almacenados, haz la solicitud a la API
+    fetch('https://sportconnect.ieti.site/profile/' + storedUsername + '/')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Data:', data);  // Imprimir los datos en la consola
+
+            localStorage.setItem('username', data.username);
+            $('#username').text(data.username);
+            // $('#city').text(data.city);
+            $('#city').text("Ciudad: " + data.city);
+            $('.titulosprofile4').text('Sobre Mí');
+
+            $('#followers-count').text(data.followers_count );  // Mostrar el conteo de seguidores en el HTML
+            $('#following-count').text(data.following_count );  // Mostrar el conteo de seguidos en el HTML
+            $('#events-count').text(data.events_count );  // Mostrar el conteo de eventos en el HTML
+
+
+            if (data.instagram != null) {
+                // Establecer el atributo src de la imagen de Instagram
+                $('#img-instagram').attr('src', './img/Profile/insta.webp');
+                // Envolver la imagen en un enlace
+                $('#img-instagram').wrap('<a href="https://www.instagram.com/' + data.instagram + '" target="_blank"></a>');
+            }
+            
+            if (data.twitter != null) {
+                // Establecer el atributo src de la imagen de Twitter
+                $('#img-twitter').attr('src', './img/Profile/twitter.webp');
+                // Envolver la imagen en un enlace
+                $('#img-twitter').wrap('<a href="https://twitter.com/' + data.twitter + '" target="_blank"></a>');
+            }
+            if (data.description != null) {
+                console.log("no es null");
+                $('#miniDescription').text(data.description);
+            }
+            var imageUrl;
+            if (data.image_path != null) {
+                imageUrl = 'https://sportconnect.ieti.site' + data.image_path;
+            } else {
+                imageUrl = 'https://sportconnect.ieti.site/Media/profile_photos/User_photo.png'; // Ruta a la imagen predeterminada
+            }
+            $('#profile-image').attr('src', imageUrl);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
     var categoriaActual = "Activos"; // Variable global para la categoría actualmente seleccionada
     $("active-events-btn").addClass("active");
     var username = localStorage.getItem("username");
